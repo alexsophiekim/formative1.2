552387448ee63c6004f9cb134371c333470b6e03
sophiekim <sophiekim@sophiekimui-MacBookPro.local>
2019-03-22 19:46:42 +1300

    edited responsive design

---
+++ js/script.js
@@ -0,0 +1,22 @@
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+
+function showNav(){
+    var menu = document.getElementById("list");
+    if(menu.style.display === 'flex'){
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'flex';
+    }
+}
